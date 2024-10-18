4d215a4aaa14e1a2c0340854cec7aba2890c60fb
Abhimanya S Ramesh <abhimanya07@gmail.com>
2024-10-18 09:30:59 +0530

    provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:dice_app/controller/dice_provider.dart';
 import 'package:dice_app/view/home_screen/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -10,6 +12,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: HomeScreen());
+    return ChangeNotifierProvider(
+      create: (context) => DiceProvider(),
+      child: const MaterialApp(home: HomeScreen()),
+    );
   }
 }
